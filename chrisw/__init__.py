5be52816333fdb13c6c868b2d0ab43ee06fb7cc7
Kang Zhang <jobo.zh@gmail.com>
2011-04-22 13:42:53 +0800

    upgrade to 0.5.6

--- chrisw/__init__.py
+++ chrisw/__init__.py
@@ -7,7 +7,7 @@ Created by Kang Zhang on 2011-02-15.
 Copyright (c) 2011 Shanghai Jiao Tong University. All rights reserved.
 """
 
-__version__ = "0.5.5α"
+__version__ = "0.5.6α"
 
 def get_version():
   """docstring for get_version"""
